2f2b26f6069d3dfd7bb8583e13b939ac6e6e3738
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-06-07 20:02:07 +0900

    first commit

--- app.py
+++ app.py
@@ -1,35 +1,47 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient()
+
+client = MongoClient("")
 db = client.dbleolego
 
-@app.route('/')
+
+@app.route("/")
 def home():
-   return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/guestbook", methods=["POST"])
+
+@app.route("/mainpage", methods=["POST"])
 def guestbook_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
+    name_receive = request.form["name_give"]
+    image_receive = request.form["image_give"]
+    price_receive = request.form["price_give"]
+    num_receive = request.form["num_give"]
+
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
+        "name": name_receive,
+        "image": image_receive,
+        "price": price_receive,
+        "num": num_receive,
     }
-    db.fan.insert_one(doc)
-    
-    return jsonify({'msg': '저장 완료!'})
 
-@app.route("/guestbook", methods=["GET"])
+    db.products.insert_one(doc)
+
+    return jsonify({"msg": "저장 완료!"})
+
+
+@app.route("/mainpage", methods=["GET"])
 def guestbook_get():
-    all_comments = list(db.fan.find({},{'_id':False}))
-    return jsonify({'result': all_comments})
+    all_product = list(db.products.find({}, {"_id": False}))
+    return jsonify({"result": all_product})
 
-# 쇼핑몰 상세 페이지 leolego03
-@app.route('/view')
-def view():
-    return render_template('view.html')
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+@app.route("/")
+def more_info():
+    return render_template("index.html")  # 여기에 상세 페이지 html 넣어야 함
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
