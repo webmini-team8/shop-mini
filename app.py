ce58c9d3bd13dc33ca266a0867b43ef627ee0c77
Taehyun Kim <agart319@gmail.com>
2023-06-08 21:37:06 +0900

    성민님꺼 추가. 태현 삭제

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ from pymongo import MongoClient
 import certifi
 
 
-ca = certifi.where()
-client = MongoClient('mongodb+srv://sparta1:test1@cluster0.kuoqp5o.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta0
-
-
-####성민님 코드 시작부분
+# ca = certifi.where()
+# client = MongoClient('mongodb+srv://sparta1:test1@cluster0.kuoqp5o.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+# db = client.dbsparta0
 # from flask import Flask, render_template, request, jsonify
 # app = Flask(__name__)
 
@@ -21,149 +18,48 @@ db = client.dbsparta0
 # client = MongoClient()
 # db = client.dbleolego
 
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-@app.route('/')
-def home():
-    return render_template('cart.html')
+# 쇼핑몰 상세 페이지 leolego03
+@app.route('/view')
+def view():
+    item_id = request.args.get('itemid')
+    temp_item = db.shop.find_one({'itemId': int(item_id)})
 
+    return render_template('view.html', temp_item = temp_item)
 
-@app.route("/itemsave", methods=["POST"])
-def item_save():  #상품 저장
+# 쇼핑몰 상세 페이지 상품 leolego03
+@app.route('/view/item', methods=["GET"])
+def view_get_item():
+    temp_item = db.shop.find_one({'itemId': 2})
 
-    userid_receive = request.form.get('userid_give', False)
-    name_receive = request.form.get('name_give', False)
-    quantity_receive = int(request.form.get('quantity_give', False))
-    catetory_receive = request.form.get('category_give', False)
-    price_receive = int(request.form.get('price_give', False))
-    imageurl_receive = request.form.get('imageurl_give', False)
-    description_receive = request.form.get('description_give, False')
-    
-    item_list = list(db.items.find({}, {'_id': False}))
-    count = len(item_list) + 1
+    return jsonify({'temp_item' : temp_item})
 
-    doc = {
-        'itemid': count,  # 등록 시, db에서 특정 상품을 찾기 위해 'num' 이라는 고유 값 부여
-        'userid': userid_receive,
-        'name': name_receive,
-        'quantity' : quantity_receive,
-        'category': catetory_receive,
-        'price': price_receive,
-        'imageurl': imageurl_receive,
-        'description': description_receive
+# 쇼핑몰 상세 페이지 상품 저장 leolego03
+@app.route('/view/item', methods=["POST"])
+def view_post_item():
+    item_count = request.form['item_count']
+
+    return jsonify({'msg': item_count + '개 추가 완료!'})
+
+# 임시 상품 등록 leolego03
+@app.route('/temp/item', methods=['GET'])
+def temp_post_item():
+    temp_item = {
+        'itemId' : 2,
+        'itemImgUrl' : 'https://cdn.pixabay.com/photo/2020/07/15/18/32/sneakers-5408674_1280.png',
+        'itemName' : '운동화',
+        'itemPrice' : 10000,
+        'itemStock' : 99,
+        'itemSubTitle': '멋있는 운동화!',
+        'itemDescription': '운동화 상세 설명입니다.'
     }
-    db.items.insert_one(doc)
 
-    return jsonify({'msg': '상품 등록이 완료되었습니다!'})
+    db.shop.insert_one(temp_item)
+    return render_template('index.html')
 
-
-
-@app.route("/itemshow", methods=["GET"]) # 아이템 모두 불러오기
-def item_get():
-    all_items = list(db.items.find({}, {'_id': False}))
-    return jsonify({'result': all_items})
-
-
-@app.route("/itemdetails", methods=["GET"])   # 아이템 하나 상세정보 모두 불러오기
-def item_details():
-    itemid_receive = request.form.get('itemid_give', False)
-
-    # 아이템 정보 찾기
-    item_details = db.items.find_one({'itemid': itemid_receive}, {'_id': False})
-
-    if item_details:
-        return jsonify({'result': item_details, 'msg': '아이템 정보가 불러와졌습니다'})
-    else:
-        return jsonify({'msg': '아이템을 찾을 수 없습니다.'})
-
-
-
-@app.route("/itembycategory", methods=["GET"]) #아이템 카테고리별로 불러오기
-def items_by_category():
-    category_receive = request.form.get('category_give', False)
-
-    # 해당 카테고리의 아이템 불러오기
-    items_in_category = list(db.items.find({'category': category_receive}, {'_id': False}))
-
-    return jsonify({'result': items_in_category, 'msg': '아이템 불러오기 성공'})
-
-#/itemsbycategory?category_give=<category>와 같이 URL의 매개변수로 category를 사용하여 이 경로를 호출해야 합니다.
-
-
-
-
-
-@app.route("/cartupdate", methods=["POST"]) #카트 업데이트 하기(수량 변동)
-def cart_change():
-    userid_receive = request.form.get('userid_give', False)
-    itemid_receive = request.form.get('itemid_give', False)
-    quantity_receive = int(request.form.get('quantity_give', False))
-    
-
-    #먼저 해당 아이디의 카트를 찾음
-    # user_cart = db.carts.find({'userid' : userid_receive}, {'_id': False})
-
-    #해당 아이템의 수량을 바꿈
-    #https://stackoverflow.com/questions/63132280/update-single-array-item-with-matching-id-and-one-of-the-array-element-using-pym
-    db.carts.update_one({'userid': userid_receive, 'cart': {'$elemMatch': { 'itemid':  itemid_receive }}}, {'$set': {'cart.$.quantity': quantity_receive}})
-    return jsonify({'msg': 'update 완료'})
-
-
-
-@app.route("/cartdelete", methods=["POST"]) #카트 아이템 삭제하기
-def cart_delete():
-    userid_receive = request.form.get('userid_give', False)
-    itemid_receive = request.form.get('itemid_give', False)
-
-    db.carts.update({'userid': userid_receive}, { '$pull' :{ 'cart': { 'itemid':  itemid_receive }}} )
-    return jsonify({'msg': '상품이 장바구니에서 삭제되었습니다.'})
-
-
-@app.route("/cartadd", methods=["POST"]) #카트 아이템 추가하기(수량추가 포함)
-def cart_add():
-    userid_receive = request.form.get('userid_give', False)
-    itemid_receive = request.form.get('itemid_give', False)
-    quantity_receive = int(request.form.get('quantity_give', False))
-
-    # 사용자의 카트 찾기
-    user_cart = db.carts.find_one({'userid': userid_receive})
-
-    # 사용자의 카트가 없으면 새로 생성하기.
-    if user_cart is None:
-        user_cart = {
-            'userid': userid_receive,
-            'cart': []
-        }
-        db.carts.insert_one(user_cart)
-
-    # 사용자의 카트에 추가하려는 아이템이 있나 확인
-    if db.carts.find_one({'userid': userid_receive, 'cart.itemid': itemid_receive}):
-        # 만약 있으면 수량만 추가
-        db.carts.update_one({'userid': userid_receive, 'cart.itemid': itemid_receive}, {'$inc': {'cart.$.quantity': quantity_receive}})
-    else:
-        # 만약 없으면 새로 추가
-        item_info = db.items.find_one({'itemid': itemid_receive}, {'_id': False})  # assuming the items collection has an item document with an 'itemid' field
-        item_info['quantity'] = quantity_receive
-        db.carts.update_one({'userid': userid_receive}, {'$push': {'cart': item_info}})
-
-    return jsonify({'msg': '상품이 장바구니에 담겼습니다.'})
-
-
-@app.route("/cartinfo", methods=["GET"])  #카트 정보 불러오기
-def cart_info():
-    userid_receive = request.form.get('userid_give', False)
-
-    # 사용자의 카트 찾기
-    user_cart = db.carts.find_one({'userid': userid_receive}, {'_id': False})
-
-    # 혹시 카트가 없으면 새로 생성하기.
-    if user_cart is None:
-        user_cart = {
-            'userid': userid_receive,
-            'cart': []
-        }
-        db.carts.insert_one(user_cart)
-
-    return jsonify({'result': user_cart, 'msg': '카트 불러오기 성공'})
 
 
 
